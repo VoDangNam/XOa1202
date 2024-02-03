de1eab9331150841a4b5969c44541f4c0e0fc5ce
VoDangNam <vodangnam30102004@gmail.com>
2024-02-03 15:53:01 +0700

    first cmm

---
+++ src/conghaisoab.java
@@ -0,0 +1,11 @@
+
+public class conghaisoab {
+
+    public static void main(String[] args) {
+            int a=1,b=2;
+            
+        System.out.println("a + b="+(a+b));
+    }
+    
+   
+}
