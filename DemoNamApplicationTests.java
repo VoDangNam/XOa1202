e9e59bb5fef1499848ad029db5cb7f3ff2bd262b
VoDangNam <140743032+VoDangNam@users.noreply.github.com>
2024-03-10 23:01:27 +0700

    Add files via upload

---
+++ DemoNamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoNam.demoNam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
